7d4a5b1dd544bfd5478eb41e882a4c065d5fc821
Tyler Grunenwald <tgrunenwald@punchthrough.com>
2024-08-30 09:43:23 -0500

    chore: remove ineffective colorscheme set

--- set.lua
+++ set.lua
@@ -17,9 +17,6 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.background = "dark"
-vim.cmd.colorscheme = "gruvbox-material"
-
 vim.g.netrw_liststyle = 3
 vim.g.netrw_banner = 0
 vim.g.netrw_preview = 1
