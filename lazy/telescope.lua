95f866b04e70341ea797ab2df3f97625c08358f1
Tyler Grunenwald <tgrunenwald@punchthrough.com>
2024-10-09 10:34:02 -0500

    feat: add more telescope bindings

--- lazy/telescope.lua
+++ lazy/telescope.lua
@@ -7,8 +7,11 @@ return {
 
 		local builtin = require('telescope.builtin')
 
+		vim.keymap.set('n', '<leader>ft', builtin.builtin, {})
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>fG', builtin.grep_string, {})
+		vim.keymap.set('n', '<leader>fG', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+		vim.keymap.set('n', '<leader>fcb', builtin.current_buffer_fuzzy_find, {})
 	end
 }
